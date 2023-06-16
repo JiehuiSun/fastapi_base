9f63e3c2408b00a343edc36393bb1a885f16ccc1
huihui <sunjiehuimail@foxmail.com>
2023-06-16 17:39:31 +0800

    Mongo RepilcaSet

--- app/core/config.py
+++ app/core/config.py
@@ -13,18 +13,22 @@ VERSION = "0.0.0"
 
 config = Config(".env")
 
+PROJECT_NAME: str = config("PROJECT_NAME", default="FastAPIBase")
+
 DEBUG: bool = config("DEBUG", cast=bool, default=False)
 
 DATABASE_URL: str = config("DB_CONNECTION", cast=str,
                            default="mongodb://localhost:27017/fastapi")
+# MONGODB Repilca Set Configuration
+MRS_READ_CONCERN: str = config("MRS_READ_CONCERN", default="majority")
+MRS_WRITE_CONCERN: str = config("MRS_WRITE_CONCERN", default="majority")
+
 MAX_CONNECTIONS_COUNT: int = config(
     "MAX_CONNECTIONS_COUNT", cast=int, default=10)
 MIN_CONNECTIONS_COUNT: int = config(
     "MIN_CONNECTIONS_COUNT", cast=int, default=10)
 
 SECRET_KEY: Secret = config("SECRET_KEY", cast=Secret)
-
-PROJECT_NAME: str = config("PROJECT_NAME", default="FastAPIBase")
 
 # logging configuration
 
--- app/models/events.py
+++ app/models/events.py
@@ -1,14 +1,21 @@
 from app.core import logger
-from app.core.config import DATABASE_URL
+from app.core.config import DATABASE_URL, MRS_READ_CONCERN, MRS_WRITE_CONCERN
 from fastapi import FastAPI
 from motor.motor_asyncio import AsyncIOMotorClient  # type: ignore
+from pymongo.read_concern import ReadConcern
+from pymongo.read_preferences import ReadPreference
+from pymongo.write_concern import WriteConcern
 
 
 async def connect_to_db(app: FastAPI) -> None:
     logger.info("Connecting to %s", DATABASE_URL)
 
     app.state.mongodb_client = AsyncIOMotorClient(DATABASE_URL)
-    app.state.db = app.state.mongodb_client.get_default_database()
+    app.state.db = app.state.mongodb_client.get_default_database(
+        read_concern=ReadConcern(MRS_READ_CONCERN),
+        write_concern=WriteConcern(w=MRS_WRITE_CONCERN),
+        read_preference=ReadPreference.PRIMARY_PREFERRED
+    )
 
     logger.info("Connection established")
 
